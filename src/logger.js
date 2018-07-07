2cb256763f9ad70d3d5826458bd5c67d5483a7cd
Blekrwen <blekrwen@waijule.com>
2018-07-07 14:17:02 +0800

    add custom logger format

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,16 @@
 import winston from 'winston';
+import isEmpty from 'lodash/isEmpty';
 
-const transports = [new winston.transports.Console()];
+const transports = [
+  new winston.transports.Console({
+    formatter: ({ message, meta, level }) => {
+      const metaString = isEmpty(meta)
+        ? ''
+        : `\n\t${JSON.stringify(meta, null, 2)}`;
+      return `[ipc-proxy] ${level}: ${message}${metaString}`;
+    },
+  }),
+];
 
 const logger = new winston.Logger({
   level: 'info',
